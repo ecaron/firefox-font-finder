efaec9287aa65e4fbabf942d47244578c310afa0
Eric Caron <eric.caron@gmail.com>
2012-10-01 21:30:18 -0500

    Initial import

---
+++ defaults/preferences/pref.js
@@ -0,0 +1,4 @@
+pref("extensions.fontfinder@bendodson.com.statusbar", true);
+pref("extensions.fontfinder@bendodson.com.contextmenu", true);
+pref("extensions.fontfinder@bendodson.com.statusBarLeftClick", false);
+pref("extensions.fontfinder@bendodson.com.analyzeLeftClick", false);
